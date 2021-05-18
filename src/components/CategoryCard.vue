8d7d295848c291a1b45bcb9378940333f5a67238
Pau Garcia Martínez <etpau@hotmail.es>
2021-05-18 17:35:03 +0200

    Create views for individual details and main composables

---
+++ src/components/CategoryCard.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    
+    <h3><slot name="category"></slot></h3>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CategoryCard'
+}
+</script>
+
+<style>
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,29 @@
 <template>
-  Home
+
+  <h1>Star Wars</h1>
+  <p>Don't miss anything</p>
+
+  <CategoryCard>
+    <template></template>
+  </CategoryCard>
+
+  <CategoryCard>
+    <template></template>
+  </CategoryCard>
+
+  <CategoryCard>
+    <template></template>
+  </CategoryCard>
+
 </template>
 
 <script>
+import CategoryCard from '@/components/CategoryCard'
+
 export default {
   name: 'Home',
   components: {
+    CategoryCard
   }
 }
 </script>
