cde2f2622ff5c57fdbc6cf960cb060ddbbd41d99
Pau Garcia Martínez <etpau@hotmail.es>
2021-05-18 17:43:05 +0200

    Set all routes

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,50 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/Home.vue'
+import People from '../views/People.vue'
+import Starships from '../views/Starships.vue'
+import Planets from '../views/Planets.vue'
+import PeopleDetails from '../views/details/PeopleDetails.vue'
+import StarshipDetails from '../views/details/StarshipDetails.vue'
+import PlanetDetails from '../views/details/PlanetDetails.vue'
 
 const routes = [
   {
     path: '/',
     name: 'Home',
     component: Home
+  },
+  {
+    path: '/people',
+    name: 'People',
+    component: People
+  },
+  {
+    path: '/people/:id',
+    name: 'PeopleDetails',
+    component: PeopleDetails,
+    props: true
+  },
+  {
+    path: '/starships',
+    name: 'Starships',
+    component: Starships
+  },
+  {
+    path: '/starships/:id',
+    name: 'StarshipDetails',
+    component: StarshipDetails,
+    props: true
+  },
+  {
+    path: '/planets',
+    name: 'Planets',
+    component: Planets
+  },
+  {
+    path: '/planets/:id',
+    name: 'PlanetDetails',
+    component: PlanetDetails,
+    props: true
   }
 ]
 
