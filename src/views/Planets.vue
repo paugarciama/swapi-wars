3cdf8cad53fb4fc74e149113a878e7cf6f5df44d
Pau Garcia Martínez <etpau@hotmail.es>
2021-05-19 19:33:17 +0200

    Start paginating

--- src/views/Planets.vue
+++ src/views/Planets.vue
@@ -9,11 +9,24 @@
     <div v-if="error">{{ error }}</div>
 
     <div v-if="planets.length">
-      <PlanetList :planets="planets" />
+      <PlanetList :planets="displayedPlanets" />
     </div>
 
     <div v-else>
       <Spinner />
+    </div>
+
+    <div class="pagination">
+      <button v-if="page !== 1" @click="page--">Before</button>
+      
+      <button 
+        v-for="pageNumber in pages" 
+        :key="pageNumber"
+        @click="page = pageNumber">
+        {{ pageNumber }}
+      </button>
+      
+      <button v-if="page < pages.length" @click="page++">Next</button>
     </div>
 
     <BackButton />
@@ -26,6 +39,7 @@ import PlanetList from '@/components/lists/PlanetList'
 import Spinner from '@/components/Spinner'
 import BackButton from '@/components/BackButton'
 import getPlanets from '@/composables/getPlanets'
+import { ref, computed } from '@vue/runtime-core'
 
 export default {
   components: { 
@@ -34,11 +48,21 @@ export default {
     BackButton 
   },
   setup() {
+    const page = ref(1)
+    const perPage = ref(10)
+    const pages = ref([1, 2, 3, 4, 5, 6])
+
     const { planets, error, load } = getPlanets()
 
     load()
 
-    return { planets, error }
+    const displayedPlanets = computed(() => {
+      let from = (page.value * perPage.value) - perPage.value
+      let to = (page.value * perPage.value)
+      return planets.value.slice(from, to)
+    })
+
+    return { planets, error, displayedPlanets, page, pages }
   }
 }
 </script>
